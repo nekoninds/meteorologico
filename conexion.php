825fb0043b3419efcbc8eaa5f417303047e3ad9a
Mumo Carlos <mumo.crls@gmail.com>
2016-11-23 19:48:15 -0600

    config mysql connection for heroku

--- conexion.php
+++ conexion.php
@@ -1,10 +1,16 @@
 <?php
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	$con = mysqli_connect("localhost","root","","estacion");
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
 
-	// Check connection
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
+  $con = new mysqli($server, $username, $password, $db);
+
+  // Check connection
+  if (mysqli_connect_errno())
+  {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 ?>
