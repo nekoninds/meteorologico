6ece4fccc3979487e32a9d4b84a7374aa31c3de4
me <hector.m.rm@hotmail.com>
2016-11-25 21:00:48 -0600

    mail

--- mail.php
+++ mail.php
@@ -2,18 +2,13 @@
 require 'vendor/autoload.php';
 
 
-$from = new SendGrid\Email(null, "test@example.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "mumo.crls@gmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$from = new SendGrid\Email(null, "support@tlaloc.com");
+$subject = "Cambio de Temperatura";
+$to = new SendGrid\Email(null, "hector.m.rm@hotmail.com");
+$content = new SendGrid\Content("text/plain", "Cambio la temperatura de Colima de 28 grados a un chingo");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = "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";
-echo $apiKey;
 $sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+$sg->client->mail()->send()->post($mail);
 ?>
