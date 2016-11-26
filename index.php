ccff2aaafc35be563dabfe2c83a08fb8370a3764
me <hector.m.rm@hotmail.com>
2016-11-26 16:31:33 -0600

    cambio de h4

--- index.php
+++ index.php
@@ -71,14 +71,14 @@ include("conexion.php");
 			</div> <!-- .site-header -->
 			<div class="antecedentes">
 				
-				<h2 style="text-align: justify center;">Historia </h2>			
-				<p><h4 style="text-align: justify; margin-left: 500px; margin-right: 500px"> 
+				<h2 style="text-align: center;">Historia </h2>			
+				<p style="text-align: justify; margin-left: 500px; margin-right: 500px"> 
 				Colima es un pequeño estado del occidente de México que colinda con Jalisco, Michoacán y el océano Pacífico. 
 				En su estrecho territorio se encierra un sinfín de atractivos y una amplia gama de hermosos lugares, como Villa de Álvarez, Comala, Suchitlán, el Parque Nacional Nevado de Colima y Volcán de Fuego, y Quesería Tecomán, y bellas playas como Manzanillo, capital mundial del pez vela, entre muchas otras.<br><br>
 
 				Tiene un clima tropical con una temperatura promedio de 25° C, subhúmedo en la mayoría del estado, con unos 1,000 mm anuales de precipitación pluvial. En la parte norte del municipio que colinda con el de Cuauhtémoc, la temperatura mejora en unos dos grados aproximadamente de 22 °C a 24°C, lluvia media anual de 1,000 a 1,300 mm.<br><br>
 
-				En los poblados Estampilla y Las Tunas, el clima es semiseco y muy cálido con temperatura de 26 a 28°C y precipitación anual de 600 a 700 mm.</p> <br> <br></h4>
+				En los poblados Estampilla y Las Tunas, el clima es semiseco y muy cálido con temperatura de 26 a 28°C y precipitación anual de 600 a 700 mm.</p> <br> <br><
 			</div>
 
 
