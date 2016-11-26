ad9270641a1aef77a499694b3b5ca04335964bc1
me <hector.m.rm@hotmail.com>
2016-11-26 01:51:04 -0600

    prueba de footer1

--- footer.php
+++ footer.php
@@ -7,16 +7,16 @@
 		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 </head>
 <body>
-	<footer class="site-footer">
-			<div class="container">
-				<div class="social-links" style="margin: 0px auto;">
+	<footer class="site-footer0">
+			<div class="container0">
+				<div class="social-links0" style="margin: 0px auto;">
 					<a href="https://www.facebook.com/" ><i class="fa fa-facebook"></i></a>
 					<a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
 					<a href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a>
 					<a href="https://es.pinterest.com/"><i class="fa fa-pinterest"></i></a>
 		
-					<p class="colophon2" style="text-align: center;">Copyright 2016 Universidad de Colima. Designed by Digital Systems. All rights reserved</p>
-					<p class="colophon">with love... 5 B ♥</p>
+					<p class="colophon0" style="text-align: center;">Copyright 2016 Universidad de Colima. Designed by Digital Systems. All rights reserved</p>
+					<p class="colophon0">with love... 5 B ♥</p>
 				</div>
 			</div>
 	</footer>
