233fd72d249c20fe1fa6d0991d9e5aa669507278
me <hector.m.rm@hotmail.com>
2016-11-26 05:36:56 -0600

    envio de datos desde contacto 6

--- mail.php
+++ mail.php
@@ -11,6 +11,6 @@ $mail = new SendGrid\Mail($from, $subject, $to, $content);
 $apiKey = "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";
 $sg = new \SendGrid($apiKey);
 $sg->client->mail()->send()->post($mail);
-
+header('Location: contact.php');
 
 ?>
