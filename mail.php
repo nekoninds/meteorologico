f83d2abe2815c97b0c994da19859d50b873e4e8b
me <hector.m.rm@hotmail.com>
2016-11-26 13:53:39 -0600

    holi

--- mail.php
+++ mail.php
@@ -1,15 +1,14 @@
 <?php
 require 'vendor/autoload.php';
-{
-$from = new SendGrid\Email(null, "$_POST['email']");
-$subject = "envio desde contacto";
+
+
+$from = new SendGrid\Email(null, "support@tlaloc.com");
+$subject = "Cambio de Temperatura";
 $to = new SendGrid\Email(null, "hector.m.rm@hotmail.com");
-$content = new SendGrid\Content("text/plain", "$_POST['msj'];");
+$content = new SendGrid\Content("text/plain", "Cambio la temperatura de Colima de 28 grados a un chingo");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = "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";
 $sg = new \SendGrid($apiKey);
 $sg->client->mail()->send()->post($mail);
-header('Location: contact.php');
-
 ?>
