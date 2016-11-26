13d0edf7f2111059cc7bb36854fc68ad3c9bb1e2
me <hector.m.rm@hotmail.com>
2016-11-26 05:41:58 -0600

    envio de datos desde contacto 7

--- mail.php
+++ mail.php
@@ -1,11 +1,10 @@
 <?php
 require 'vendor/autoload.php';
-if ($_SERVER['ŔEQUEST_METHOD'] == 'POST')
 {
-$from = new SendGrid\Email(null, "echo $_POST['email'];");
+$from = new SendGrid\Email(null, "$_POST['email']");
 $subject = "envio desde contacto";
 $to = new SendGrid\Email(null, "hector.m.rm@hotmail.com");
-$content = new SendGrid\Content("text/plain", "echo $_POST['msj'];");
+$content = new SendGrid\Content("text/plain", "$_POST['msj'];");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = "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";
