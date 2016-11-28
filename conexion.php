0d23564e16266fd0fee1049a5ea6aae9df223c27
me <hector.m.rm@hotmail.com>
2016-11-27 20:22:29 -0600

    redoo

--- conexion.php
+++ conexion.php
@@ -1,7 +1,5 @@
 <?php
   $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo $url;
-echo getenv("SENDGRID_API_KEY");
   $server = $url["host"];
   $username = $url["user"];
   $password = $url["pass"];
